42da2184b6bf9d36d4bc93fccc92490e628629b4
nakyung <zzum0701@naver.com>
2023-10-23 12:16:03 +0900

    초기 커밋

---
+++ src/main/java/kr/lfin/exam/LfinDevExamApplication.java
@@ -0,0 +1,13 @@
+package kr.lfin.exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LfinDevExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LfinDevExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/lfin/exam/LfinDevExamApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.lfin.exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LfinDevExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
