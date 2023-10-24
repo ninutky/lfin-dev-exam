662c51a8a69887eb40ef7948d1f6a8d8bb0cdeba
nakyung <zzum0701@naver.com>
2023-10-24 17:47:34 +0900

    비밀번호 암호화 알고리즘을 BCrypt로 변경

---
+++ src/main/java/kr/lfin/exam/common/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package kr.lfin.exam.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(AbstractHttpConfigurer::disable)
+                .authorizeRequests()
+                .requestMatchers("/signup", "/list").permitAll()
+                .and()
+                .build();
+    }
+
+    @Configuration
+    public class EncoderConfig {
+        @Bean
+        public BCryptPasswordEncoder encoder() {
+            return new BCryptPasswordEncoder();
+        }
+    }
+}
--- src/main/java/kr/lfin/exam/domains/User.java
+++ src/main/java/kr/lfin/exam/domains/User.java
@@ -4,7 +4,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-import org.apache.commons.codec.digest.DigestUtils;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 
 @Entity
 //@Table(name = "user")
@@ -21,14 +21,14 @@ public class User {
     public static User createUser(String email, String password, String name, String phone){
         User user = new User();
         user.setEmail(email);
-        user.setPassword(SHA256(password)); // 암호를 해시로 변환
+        user.setPassword(bcryptHashPassword(password)); // 암호를 해시로 변환
         user.setName(name);
         user.setPhone(phone);
         return user;
     }
 
-    public static String SHA256(String password){
-        return DigestUtils.sha256Hex(password);
+    public static String bcryptHashPassword(String password){
+        return BCrypt.hashpw(password, BCrypt.gensalt());
     }
 
 }
