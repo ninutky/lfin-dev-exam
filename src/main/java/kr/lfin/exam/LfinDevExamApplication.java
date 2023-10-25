817abc5388bf68db1be8171ffb159c5291b053ff
nakyung <zzum0701@naver.com>
2023-10-25 18:41:49 +0900

    Pageable의 page를 1부터 시작하도록 수정

--- src/main/java/kr/lfin/exam/LfinDevExamApplication.java
+++ src/main/java/kr/lfin/exam/LfinDevExamApplication.java
@@ -2,7 +2,9 @@ package kr.lfin.exam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
@@ -10,6 +12,14 @@ public class LfinDevExamApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LfinDevExamApplication.class, args);
+	}
+
+	@Bean
+	public PageableHandlerMethodArgumentResolverCustomizer customizePageable() {
+		return p -> {
+			p.setOneIndexedParameters(true);
+			p.setMaxPageSize(10);
+		};
 	}
 
 }
