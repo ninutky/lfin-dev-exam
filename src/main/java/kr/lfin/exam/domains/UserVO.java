be269873722d4a3e39eb37d8413cb3098ceca076
nakyung <zzum0701@naver.com>
2023-10-27 17:54:38 +0900

    UserVO 어노테이션 수정

--- src/main/java/kr/lfin/exam/domains/UserVO.java
+++ src/main/java/kr/lfin/exam/domains/UserVO.java
@@ -9,9 +9,9 @@ import lombok.Setter;
 public class UserVO {
     private Long uId;
     private String email;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private String password;
     private String name;
     private String phone;
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Boolean deleted;
 }
