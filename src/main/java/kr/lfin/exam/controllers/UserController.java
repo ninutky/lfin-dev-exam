9285bb8c29620fd102ba536006d2d5278d9fb9c3
nakyung <zzum0701@naver.com>
2023-10-31 15:32:10 +0900

    컨트롤러를 수정하여 /v1/user 엔드포인트에서 요청을 처리하도록 변경

--- src/main/java/kr/lfin/exam/controllers/UserController.java
+++ src/main/java/kr/lfin/exam/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/v1/user")
 @RequiredArgsConstructor
 @Tag(name = "User", description = "User management APIs")
 public class UserController {
